ba2218fc95600186f1b1b215c10fe03fdca655bd
grigorijtomczuk <grigorijtomczuk@gmail.com>
2023-10-08 20:00:50 +0300

    08-oct-23

--- cs/lab02/lab02.py
+++ cs/lab02/lab02.py
@@ -1,19 +1,33 @@
-num = None
+while True:
+	input_number = None
 
-while not num:
-	try:
-		num = int(input("Введите число N: "))
-		if num == 0:
-			break
-	except ValueError:
-		print("Пожалуйста, введите целое число.")
+	while not input_number:
+		try:
+			input_number = int(input("Введите число N: "))
+			if input_number == 0: break
+		except ValueError:
+			print("Пожалуйста, введите целое число.")
 
-power2 = 0
+	for base in range(10):
+		exponent = 1
 
-while 2**power2 < num:
-	power2 += 1
+		# Исключение для 0 и 1
+		if base == 0:
+			if input_number >= 1: exponent = 0
+			else: exponent = 1
+			print(f"Число {str(base)} - ", end="")
+			print(f"степень: {exponent}, число в степени: {base**exponent}")
+			continue
+		elif base == 1: pass
+		else:
+			# Увеличиваем показатель степени на 1, пока число base в степени не превысит число N
+			while base**exponent < input_number:
+				exponent += 1
 
-if 2**power2 - num <= num - 2**power2 // 2:
-	print(f"Степень: {power2}, два в степени: {2**power2}")
-else:
-	print(f"Степень: {power2 // 2}, два в степени: {2**(power2 // 2)}")
+		print(f"Число {str(base)} - ", end="")
+
+		# Проверяем, не ближе ли к числу N число base в степени, меньшей на единицу (меньше N)
+		if input_number - base**(exponent - 1) <= base**exponent - input_number:
+			exponent -= 1
+
+		print(f"степень: {exponent}, число в степени: {base**exponent}")
