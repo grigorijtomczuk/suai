bca6f0fe9801a55d0725a14e733654b9e6ff4c90
grigorijtomczuk <grigorijtomczuk@gmail.com>
2023-12-06 22:20:29 +0300

    06-dec-23-7

--- cs/lab07/components/teachers/service.py
+++ cs/lab07/components/teachers/service.py
@@ -4,39 +4,34 @@ from utils import json_service
 def get_one_by_id(id):
   db = json_service.get_database()
 
-  for elem in db["teachers"]:
-    if elem["id"] == id:
-      return elem
+  for item in db["teachers"]:
+    if item["id"] == id:
+      return item
 
   return {"message": f"Элемент с {id} не найден"}
 
 
 def get_all():
   db = json_service.get_database()
-
   return db["teachers"]
 
 
 def create_one(teacher):
   db = json_service.get_database()
-
-  last_teacher_id = get_all()[-1]["id"]
+  last_teacher_id = get_all()[-1]["id"] if get_all() else 0
   db["teachers"].append({"id": last_teacher_id + 1, **teacher})
-
   json_service.set_database(db)
 
 
 def update_one_by_id(id, teacher):
   db = json_service.get_database()
 
-  for i, elem in enumerate(db["teachers"]):
-    if elem["id"] == id:
-
-      elem["name"] = teacher["name"]
-      elem["contacts"] = teacher["contacts"]
-
+  for item in db["teachers"]:
+    if item["id"] == id:
+      item["name"] = teacher["name"]
+      item["contacts"] = teacher["contacts"]
       json_service.set_database(db)
-      return elem
+      return item
 
   return {"message": f"Элемент с {id} не найден"}
 
@@ -44,12 +39,10 @@ def update_one_by_id(id, teacher):
 def delete_one_by_id(id):
   db = json_service.get_database()
 
-  for i, elem in enumerate(db["teachers"]):
-    if elem["id"] == id:
-
+  for i, item in enumerate(db["teachers"]):
+    if item["id"] == id:
       candidate = db["teachers"].pop(i)
       json_service.set_database(db)
-
       return candidate
 
   return {"message": f"Элемент с {id} не найден"}
--- cs/lab07/main.py
+++ cs/lab07/main.py
@@ -6,29 +6,33 @@ from components.teachers import service as teacher
 
 format_json = lambda obj: json.dumps(obj, indent=2, ensure_ascii=False)
 
-operation = input("Выберите операцию: ")
+while True:
+  operation = input("Выберите операцию: ")
 
-match operation:
-  case "1":
-    print(format_json(teacher.get_all()))
-  case "2":
-    print(format_json(teacher.get_one_by_id(1)))
-  case "3":
-    teacher.create_one({
-      "name": "Тест Тест",
-      "courses_id": [1, 2],
-      "contacts": {
-        "email": "тест@example.com",
-        "phone": "+333333",
-      },
-    })
-  case "4":
-    print(teacher.update_one_by_id(4, {
-      "name": "Не Смирнова Екатерина",
-      "contacts": {
-        "email": "нет@example.com",
-        "phone": "+1122334455",
-      },
-    }))
-  case "5":
-    print(teacher.delete_one_by_id(4))
+  match operation:
+    case "1":
+      print(format_json(teacher.get_all()))
+    case "2":
+      id = int(input("Введите ID: "))
+      print(format_json(teacher.get_one_by_id(id)))
+    case "3":
+      teacher.create_one({
+        "name": "Тест Тест",
+        "courses_id": [1, 2],
+        "contacts": {
+          "email": "тест@example.com",
+          "phone": "+333333",
+        },
+      })
+    case "4":
+      id = int(input("Введите ID: "))
+      print(format_json(teacher.update_one_by_id(id, {
+        "name": "Не Смирнова Екатерина",
+        "contacts": {
+          "email": "нет@example.com",
+          "phone": "+1122334455",
+        },
+      })))
+    case "5":
+      id = int(input("Введите ID: "))
+      print(format_json(teacher.delete_one_by_id(id)))
