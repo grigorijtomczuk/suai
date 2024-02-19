92928db3c4d485f9d4445f65d201f4aa34936996
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-02-19 11:19:40 +0300

    19-feb-24-2

--- op/lab1.1/main.cpp
+++ op/lab1.1/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    int P, a, b, R;
+    std::string P;
+    int a, b, R;
+
     std::cin >> P;
     std::cin >> a;
     std::cin >> b;
     std::cin >> R;
+
+    if (true)
+    {
+        std::cout << "True" << std::endl;
+    }
+
     std::cout << P << a << b << R;
+
     return 0;
 }
