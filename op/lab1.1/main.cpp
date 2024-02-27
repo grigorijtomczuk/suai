a8c2fff88abe427fad89c1c8b27d6cea16c0e8d6
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-02-27 03:06:20 +0300

    27-feb-24

--- op/lab1.1/main.cpp
+++ op/lab1.1/main.cpp
@@ -1,27 +1,66 @@
 #include <iostream>
-#include <string>
-#include <bits/stdc++.h>
+#include <cmath>
+#include <windows.h>
 
+struct Point2D {
+    short x;
+    short y;
+};
 
-void tokenizer(std::string s, char del) {
-    std::stringstream ss(s);
-    std::string word;
-    while (!ss.eof()) {
-        getline(ss, word, del);
-        std::cout << word << std::endl;
-    }
-}
+int func(Point2D point, short a, short b, unsigned short R);
 
 int main() {
-    std::string P;
-    int a, b, R;
+    SetConsoleOutputCP(CP_UTF8);
 
-    std::cin >> P;
+    short x, y, a, b;
+    unsigned short R;
+
+    std::cout << "x: ";
+    std::cin >> x;
+
+    std::cout << "y: ";
+    std::cin >> y;
+
+    std::cout << "a: ";
     std::cin >> a;
+
+    std::cout << "b: ";
     std::cin >> b;
+
+    std::cout << "R: ";
     std::cin >> R;
 
-    tokenizer(P, ',');
+    if (std::cin.fail()) {
+        std::cerr << "Введите целое число" << std::endl;
+        return 1;
+    }
+
+    if (a < b) {
+        std::cerr << "Должно выполнятся условие: a > b" << std::endl;
+        return 1;
+    }
+
+    int result = func({x, y}, a, b, R);
+
+    std::cout << "Область: " << result << std::endl;
 
     return 0;
+}
+
+int func(Point2D point, short a, short b, unsigned short R) {
+    if (pow(point.x, 2) + pow(point.y, 2) > pow(R, 2)) {
+        return 0;
+    } else if (pow(point.x, 2) + pow(point.y, 2) < pow(R, 2)) {
+        if (point.y > a) {
+            return 1;
+        }
+        if (point.y < a && point.y > b) {
+            return 2;
+        }
+        if (point.y < b) {
+            return 3;
+        }
+    }
+    std::cout << "Точка находится на одной из линий" << std::endl;
+    exit(0);
 }
