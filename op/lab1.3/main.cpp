a3cc38a8fda34c1db63c5664ac35d4b7ddcd3687
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-03-26 13:20:12 +0300

    26-mar-24

---
+++ op/lab1.3/main.cpp
@@ -0,0 +1,17 @@
+#include <cstddef>
+
+bool read_array(const char *filename, long *&array, size_t &size) {
+    return true;
+}
+
+bool write_array(const char *filename, long *array, size_t &size) {
+    return true;
+}
+
+void calculation(long *input_array, size_t input_size, long *&output_array, size_t &output_size) {
+    return;
+}
+
+int main() {
+    return 0;
+};
