7806b7e29c9bdf74fe712eb1b543e79ac056e094
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-04-23 17:35:50 +0300

    23-apr-24

--- op/lab1.2/main.cpp
+++ op/lab1.2/main.cpp
@@ -2,24 +2,52 @@
 // что в них нет цифры B в младшем разряде десятичной записи.
 
 #include <iostream>
+#include <cmath>
+#include <vector>
 
-bool is_whole(size_t number) {
-    return floor(number) == number;
-};
+// bool is_prime(int number) {
+//     static int divisors[100] = {};
+//     int i = 0;
+//
+//     for (int divisor = 2; divisor <= sqrt(number); divisor++) {
+//         int remainder = number % divisor;
+//         if (remainder == 0) {
+//             divisors[i] = divisor;
+//             i++;
+//         }
+//     }
+//
+//     return divisors[0] == 0;
+// };
+//
+// int *find_divisors(int A, int B) {
+//     static int divisors[100] = {};
+//     int i = 0;
+//
+//     for (int divisor = 2; divisor <= A; divisor++) {
+//         int remainder = A % divisor;
+//         if (remainder == 0 && is_prime(divisor)) {
+//             divisors[i] = divisor;
+//             i++;
+//         }
+//     }
+//
+//     return divisors;
+// }
 
-int *find_dividers(int A, int B) {
-    static int dividers[100] = {};
-    int i = 0;
-
-    for (int divider = 0; divider < A; divider++) {
-        size_t quotient = A % divider;
-        if (is_whole(quotient)) {
-            dividers[i] = quotient;
-            i++;
+std::vector<int> find_divisors(int A, int B) {
+    for (int i = 2; i < A; ++i) {
+        int flag = 1;
+        for (int j = 2; j < i; ++j)
+            if (i % j == 0 && i != j) {
+                flag = 0;
+                break;
+            }
+        if (flag == 1 && A % i == 0) {
+            std::cout << i << std::endl;
+            A /= i;
         }
     }
-
-    return dividers;
 }
 
 int main() {
@@ -31,12 +59,9 @@ int main() {
     std::cout << "B: ";
     std::cin >> B;
 
-    int *result = find_dividers(A, B);
+    std::vector<int> result = find_divisors(A, B);
 
-    for (int i = 0;; i++) {
-        if (result[i] == 0) {
-            break;
-        }
+    for (int i = 0; i < result.size(); i++) {
         std::cout << result[i] << std::endl;
     }
 
