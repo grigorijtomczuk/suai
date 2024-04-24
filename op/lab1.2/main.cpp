440187a8bdd3f94813cee3a0da6137dddb55cb36
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-04-24 04:02:18 +0300

    24-apr-24-2

--- op/lab1.2/main.cpp
+++ op/lab1.2/main.cpp
@@ -2,52 +2,23 @@
 // что в них нет цифры B в младшем разряде десятичной записи.
 
 #include <iostream>
-#include <cmath>
 #include <vector>
 
-// bool is_prime(int number) {
-//     static int divisors[100] = {};
-//     int i = 0;
-//
-//     for (int divisor = 2; divisor <= sqrt(number); divisor++) {
-//         int remainder = number % divisor;
-//         if (remainder == 0) {
-//             divisors[i] = divisor;
-//             i++;
-//         }
-//     }
-//
-//     return divisors[0] == 0;
-// };
-//
-// int *find_divisors(int A, int B) {
-//     static int divisors[100] = {};
-//     int i = 0;
-//
-//     for (int divisor = 2; divisor <= A; divisor++) {
-//         int remainder = A % divisor;
-//         if (remainder == 0 && is_prime(divisor)) {
-//             divisors[i] = divisor;
-//             i++;
-//         }
-//     }
-//
-//     return divisors;
-// }
-
 std::vector<int> find_divisors(int A, int B) {
+    std::vector<int> divisors;
     for (int i = 2; i < A; ++i) {
-        int flag = 1;
+        bool is_prime = true;
         for (int j = 2; j < i; ++j)
             if (i % j == 0 && i != j) {
-                flag = 0;
+                is_prime = false;
                 break;
             }
-        if (flag == 1 && A % i == 0) {
-            std::cout << i << std::endl;
+        if (is_prime && A % i == 0 && i % 10 != B) {
+            divisors.push_back(i);
             A /= i;
         }
     }
+    return divisors;
 }
 
 int main() {
