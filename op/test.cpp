b0f12976994913fdf3db8900d784d5216fc29592
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-05-15 20:30:26 +0300

    15-may-24

--- op/test.cpp
+++ op/test.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
-#include <cmath>
-#include <set>
+
+using namespace std;
+
+int main() {
+    int num = 5;
+    int *num_ptr = &num;
+    cout << &num << endl;
+    cout << num_ptr << endl;
+    cout << num << endl;
+    cout << sizeof(num) << endl;
+
+    char str[5] = "abcd";
+    short s_value = 0;
+
+    return 0;
+}
--- op/test1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int test = 3;
-    while (test > 1) {
-        std::cout << test << std::endl;
-        test--;
-    }
-}
