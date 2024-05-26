42dcc6788cc89cc008b683f132ab7c7eb8be810b
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-05-26 19:22:24 +0300

    26-may-24-3

--- op/lab2.2/main2.2.cpp
+++ op/lab2.2/main2.2.cpp
@@ -7,17 +7,17 @@
 std::vector<std::string> split_words(const std::string &string) {
     std::vector<std::string> words;
     std::string word;
-    for (char ch : string) {
-        if (ch == ' ' || ch == ',' || ch == '\n') {
+    for (char character : string) {
+        if (character == ' ' || character == ',' || character == '\n') {
             if (!word.empty()) {
                 words.push_back(word);
                 word.clear();
             }
         } else {
-            word += ch;
+            word += character;
         }
     }
-    if (!word.empty()) {
+    if (!word.empty()) { // Добавляем последнее слово
         words.push_back(word);
     }
     return words;
@@ -25,10 +25,13 @@ std::vector<std::string> split_words(const std::string &string) {
 
 std::vector<int> find_indices(const std::string &string, const std::string &word) {
     std::vector<int> indices;
-    size_t position = string.find(word, 0);
+    size_t position = string.find(word); // Находим первое вхождение
     while (position != std::string::npos) {
-        indices.push_back(static_cast<int>(position));
-        position = string.find(word, position + 1);
+        char character = string[position + word.size()];
+        // Проверяем окончание слова
+        if (character == ' ' || character == ',' || character == '\n' || character == '\0')
+            indices.push_back((int) position); // Добавляем индекс в вектор
+        position = string.find(word, position + 1); // Ищем следующее вхождение, начиная с позиции position + 1
     }
     return indices;
 }
@@ -49,22 +52,26 @@ int main() {
     std::cout << "S2: ";
     std::getline(std::cin >> std::ws, S2);
 
+    std::string lower_s1 = transform_to_lower(S1);
+    std::string lower_s2 = transform_to_lower(S2);
+
     std::unordered_map<std::string, std::vector<int>> word_indices_s1;
     std::unordered_map<std::string, std::vector<int>> word_indices_s2;
 
     std::vector<std::string> words_s1 = split_words(S1);
     std::vector<std::string> words_s2 = split_words(S2);
 
+    // Находим индексы
     for (const std::string &word : words_s1) {
         std::string lower_word = transform_to_lower(word);
-        word_indices_s1[lower_word] = find_indices(transform_to_lower(S1), lower_word);
+        word_indices_s1[lower_word] = find_indices(lower_s1, lower_word);
     }
-
     for (const std::string &word : words_s2) {
         std::string lower_word = transform_to_lower(word);
-        word_indices_s2[lower_word] = find_indices(transform_to_lower(S2), lower_word);
+        word_indices_s2[lower_word] = find_indices(lower_s2, lower_word);
     }
 
+    // Находим общие слова
     std::unordered_set<std::string> common_words;
     for (const auto &pair : word_indices_s1) {
         if (word_indices_s2.find(pair.first) != word_indices_s2.end()) {
@@ -72,6 +79,7 @@ int main() {
         }
     }
 
+    // Выводим индексы вхождений
     for (const std::string &word : common_words) {
         std::vector<int> current_indices_s1 = word_indices_s1[word];
         std::vector<int> current_indices_s2 = word_indices_s2[word];
