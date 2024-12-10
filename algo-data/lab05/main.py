7df3f362d4fc9298e38f10543b6322c49cc4cb9a
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-12-11 00:08:06 +0300

    11-dec-24

--- algo-data/lab05/main.py
+++ algo-data/lab05/main.py
@@ -1,20 +1,48 @@
-from collections import deque
+class MyDeque:
+    def __init__(self):
+        self.items = []
+
+    def __bool__(self):
+        return bool(self.items)
+
+    def is_empty(self):
+        return self.items == []
+
+    def append(self, item):
+        self.items.append(item)
+
+    def appendleft(self, item):
+        self.items.insert(0, item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def popleft(self):
+        return self.items.pop(0)
+
+    def getleft(self):
+        if self.is_empty(): return
+        return self.items[0]
+
+    def getright(self):
+        if self.is_empty(): return
+        return self.items[-1]
 
 
 # Находит максимум в каждом окне размера m массива array.
 def find_max_sliding_window(n, array, m):
     # Дек для хранения индексов элементов текущего окна
-    deque_indices = deque()
+    deque_indices = MyDeque()
     max_in_windows = []
 
     for i in range(n):
         # Удаляем элементы, которые выходят за пределы окна
-        if deque_indices and deque_indices[0] < i - m + 1:
+        if deque_indices and deque_indices.getleft() < i - m + 1:
             deque_indices.popleft()
 
         # Удаляем из дека все элементы, которые меньше текущего
         # Так как они не могут быть максимумом в текущем или последующих окнах
-        while deque_indices and array[deque_indices[-1]] < array[i]:
+        while deque_indices and array[deque_indices.getright()] < array[i]:
             deque_indices.pop()
 
         # Добавляем текущий индекс в дек
@@ -23,7 +51,7 @@ def find_max_sliding_window(n, array, m):
         # Максимум окна находится в начале дека
         # Начинаем добавлять максимумы в список только после того, как заполнили первое окно
         if i >= m - 1:
-            max_in_windows.append(array[deque_indices[0]])
+            max_in_windows.append(array[deque_indices.getleft()])
 
     return max_in_windows
 
