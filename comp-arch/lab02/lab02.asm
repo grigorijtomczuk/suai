2b6afa98b21ab316e1f6ccaea9c9213d58532132
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2025-12-18 11:40:29 +0300

    18-dec-25-1

--- comp-arch/lab02/lab02.asm
+++ comp-arch/lab02/lab02.asm
@@ -1,5 +1,5 @@
 ; input
-MOV AX, 0FFF9h    ; X1
+MOV AL, 0F9h      ; X1
 MOV BX, 000B6h    ; X2
 MOV CX, 0FF30h    ; X3
 MOV DX, 0001Ah    ; X4
@@ -7,12 +7,13 @@ MOV DX, 0001Ah    ; X4
 ; X3 = X3 + X2 
 ADD CX, BX        ; CX = X3 + X2
 
-; DX:AX = X1 / X3 
-CWD               ; sign-extend AX in DX:AX (if AX<0: DX = FFFFh, else zero) - IDIV takes 32-bit DX:AX as devidend 
+; DX:AL = X1 / X3     
+CBW
+CWD               ; sign-extend AL in DX:AX (if AL<0: DX = FFFFh, else zero) - IDIV takes 32-bit DX:AL as devidend 
 IDIV CX           ; (DX:AX) / CX -> AX=quotient, DX=remainder
 
 ; X2 = X2 XOR X1 
-MOV AX, 0FFF9h    ; AX = X1 after IDIV
+MOV AL, 0F9h      ; AL = X1 after IDIV
 XOR BX, AX        ; BX = X2 XOR X1
 
 ; X3 = X3 - X1 - CF 
